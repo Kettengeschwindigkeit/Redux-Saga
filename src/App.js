77bad6c44abe02276613377f2e5d56a91751e428
Kettengeschwindigkeit <666damned999@mail.ru>
2023-01-27 13:37:40 +0300

    Redux Integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCount, decreaseCount } from "./redux/actions/actionCreator";
 
 const App = () => {
-  const store = useSelector(store => store)
-  console.log(store)
+  const count = useSelector(store => store?.counter?.count);
+  const dispatch = useDispatch();
 
-  return <h1>Hello world!</h1>
+  const handleIncrease = () => {
+    dispatch(increaseCount());
+  }
+
+  const handleDecrease = () => {
+    dispatch(decreaseCount());
+  }
+
+  return (
+    <div>
+      <button onClick={handleIncrease}>+1</button>
+      <button onClick={handleDecrease}>+1</button>
+      <h1>{count}</h1>
+    </div>
+  );
 };
 
 export default App;
+
+// ===========================================================================================================================================================================================================
+
+// import { useState } from "react";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const handleIncrease = () => {
+//     setCount(count + 1);
+//   }
+
+//   const handleDecrease = () => {
+//     setCount(count - 1);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleIncrease}>+1</button>
+//       <button onClick={handleDecrease}>+1</button>
+//       <h1>{count}</h1>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import reducer from "./reducers/index";
 
 const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-const configureStore = preloadedState => createStore(reducer, preloadedState, composeEnhancers());
+const configureStore = preloadedState => createStore(
+    reducer,
+    preloadedState,
+    composeEnhancers()
+);
 
 const store = configureStore({});
 
